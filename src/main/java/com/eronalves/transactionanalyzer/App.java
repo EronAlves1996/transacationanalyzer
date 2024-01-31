c3a0ddfe6a480abd8fb9542cc56eddb502cdb587
EronAlves1996 <eron.alves@rocketmail.com>
2024-01-31 00:24:54 -0300

    feat: create streams queries

--- src/main/java/com/eronalves/transactionanalyzer/App.java
+++ src/main/java/com/eronalves/transactionanalyzer/App.java
@@ -1,6 +1,10 @@
 package com.eronalves.transactionanalyzer;
 
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import com.eronalves.transactionanalyzer.model.Trader;
@@ -26,5 +30,38 @@ public class App {
         new Transaction(carlos, 2020, 300.40f, Currency.JPY),
         new Transaction(joao, 2014, 15.14f, Currency.AUR),
         new Transaction(leo, 2023, 15.23f, Currency.BRL));
+
+    List<Transaction> transactions2023SortedByValue = transactions.get()
+        .filter(t -> t.getYear() == 2023)
+        .sorted(Comparator.comparing(Transaction::getValue))
+        .collect(Collectors.toList());
+
+    double usdTotalValues = transactions.get()
+        .filter(t -> t.getCurrency().equals(Currency.USD))
+        .collect(Collectors.summingDouble(Transaction::getValue));
+
+    Transaction smallestValueTransaction = transactions.get()
+        .min(Comparator.comparing(Transaction::getValue))
+        .orElse(null);
+
+    Function<Transaction, Trader> getTrader = Transaction::getTrader;
+
+    transactions
+        .get()
+        .filter(t -> t.getTrader().getCity().equals("Tokyo"))
+        .map(getTrader
+            .andThen(Trader::getName))
+        .forEach(System.out::println);
+
+    boolean isAnyTraderBasedOnSaoPaulo = transactions.get()
+        .anyMatch(t -> t.getTrader().getCity().equals("São Paulo"));
+
+    double highestValueTransactionInJPY = transactions
+        .get()
+        .filter(t -> t.getCurrency().equals(Currency.JPY))
+        .mapToDouble(Transaction::getValue)
+        .max()
+        .orElse(0);
+
   }
 }
