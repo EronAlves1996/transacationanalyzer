8d0293516e5e2555d908bd2ed7bdd999aeab23f6
EronAlves1996 <eron.alves@rocketmail.com>
2024-01-30 23:52:12 -0300

    feat: create domain model

---
+++ src/main/java/com/eronalves/transactionanalyzer/model/Transaction.java
@@ -0,0 +1,40 @@
+package com.eronalves.transactionanalyzer.model;
+
+/**
+ * Transaction
+ */
+public class Transaction {
+
+  private final Trader trader;
+  private final int year;
+  private final float value;
+  private final Currency currency;
+
+  public Transaction(Trader trader, int year, float value, Currency currency) {
+    this.trader = trader;
+    this.year = year;
+    this.value = value;
+    this.currency = currency;
+  }
+
+  public enum Currency {
+    USD, BRL, YEN, AUR
+  }
+
+  public Trader getTrader() {
+    return trader;
+  }
+
+  public int getYear() {
+    return year;
+  }
+
+  public float getValue() {
+    return value;
+  }
+
+  public Currency getCurrency() {
+    return currency;
+  }
+
+}
